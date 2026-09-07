425246acc52876f3c8368dd7aaba912951e4ca05
Your Name <you@example.com>
2026-09-07 12:52:09 +0330

    3.10.2

--- src/TokenCreate.rs
+++ src/TokenCreate.rs
@@ -96,14 +96,14 @@ pub fn vectok(vector: BitVec<u8, Msb0>) -> Vec<Token> {
             i = _i;
             continue;
         }
-        if vector[i] == false && vector[i+1] == true && vector[i+2] == false && vector[i+3] == true && vector[i+4] == false {
+        if vector[i] == false && vector[i+1] == true && vector[i+2] == false && vector[i+3] == false && vector[i+4] == true {
             i += 5;
             let (ipnumber, _i) = read_to_u64(&vector, &bss, i);
             tokens.push(Token::Do_IF(ipnumber));
             i = _i;
             continue;
         }
-        if vector[i] == false && vector[i+1] == true && vector[i+2] == false && vector[i+3] == true && vector[i+4] == true {
+        if vector[i] == false && vector[i+1] == true && vector[i+2] == false && vector[i+3] == true && vector[i+4] == false {
             i += 5;
             let result = match (vector[i], vector[i+1]) {
                 (false, false) => XOR,
@@ -116,19 +116,19 @@ pub fn vectok(vector: BitVec<u8, Msb0>) -> Vec<Token> {
             i += 2;
             continue;
         }
-        if !vector[i] && vector[i+1] && vector[i+2] && !vector[i+3] && !vector[i+4] {
+        if !vector[i] && vector[i+1] && !vector[i+2] && vector[i+3] && vector[i+4] {
             tokens.push(Token::Done);
             i += 5;
             continue;
         }
-        if !vector[i] && vector[i+1] && vector[i+2] && !vector[i+3] && vector[i+4] {
+        if !vector[i] && vector[i+1] && vector[i+2] && !vector[i+3] && !vector[i+4] {
             i += 5;
             let (data, _i) = read_to_u64(&vector, &bss, i);
             i = _i;
             tokens.push(Token::Duplicate_Select(data));
             continue
         }
-        if vector[i] == false && vector[i+1] == true && vector[i+2] == true && vector[i+3] == true && vector[i+4] == false {
+        if vector[i] == false && vector[i+1] == true && vector[i+2] == true && vector[i+3] == false && vector[i+4] == true {
             i += 5;
             let (fd, i0) = read_to_u64(&vector, &bss, i);
             let (sd, i1) = read_to_u64(&vector, &bss, i0);
@@ -136,21 +136,21 @@ pub fn vectok(vector: BitVec<u8, Msb0>) -> Vec<Token> {
             tokens.push(Token::swap_Select(fd, sd));
             continue;
         }
-        if vector[i] == false && vector[i+1] == true && vector[i+2] == true && vector[i+3] == true && vector[i+4] == true {
+        if vector[i] == false && vector[i+1] == true && vector[i+2] == true && vector[i+3] == true && vector[i+4] == false {
             i += 5;
             let (data, _i) = read_to_u64(&vector, &bss, i);
             tokens.push(Token::swap_select_to_last(data));
             i = _i;
             continue;
         }
-        if vector[i] == true && vector[i+1] == false && vector[i+2] == false && vector[i+3] == false && vector[i+4] == false {
+        if vector[i] == false && vector[i+1] == true && vector[i+2] == true && vector[i+3] == true && vector[i+4] == true {
             i += 5;
             let (data, _i) = read_to_u64(&vector, &bss, i);
             tokens.push(Token::call(data));
             i = _i;
             continue;
         }
-        if vector[i] == true && vector[i+1] == false && vector[i+2] == false && vector[i+3] == false && vector[i+4] == true {
+        if vector[i] == true && vector[i+1] == false && vector[i+2] == false && vector[i+3] == false && vector[i+4] == false{
             i += 5;
             tokens.push(Token::ret);
             continue;
@@ -172,7 +172,7 @@ pub fn vectok(vector: BitVec<u8, Msb0>) -> Vec<Token> {
             continue;
 
         }
-        if vector[i] == true && vector[i+1] == false && vector[i+2] == false && vector[i+3] == true && vector[i+4] == true {
+        if vector[i] == true && vector[i+1] == false && vector[i+2] == false && vector[i+3] == true && vector[i+4] == false {
             i += 5;
             if vector[i] == false {
                 tokens.push(Token::clear(onStack));
